fced690cbf0741066ac5f6cefd61d59c5ec3b90f
Martin Abelson Sahlen <Sahlen@Martins-MacBook-Pro-2.local>
2015-11-15 12:30:53 +0100

    Fix java version

--- app/auth/SecuredAction.scala
+++ app/auth/SecuredAction.scala
@@ -6,6 +6,7 @@ import scala.concurrent.Future
 
 object SecuredAction extends ActionBuilder[Request] {
   def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+    print(request.headers);
     block(request)
   }
 }
